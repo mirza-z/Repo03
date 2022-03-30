7c73167a4a79ed7efae9c98161e050ba4869b91b
mirza-z <zaimovic330@gmail.com>
2022-03-30 14:09:04 +0200

    Added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line
+    //This is my first comment
+    /*This is my second comment*/
+    //Only change code above this line
+
+}
+
+main();
+
+module.exports = main;
